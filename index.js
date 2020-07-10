9a4f5aef023a06e52fbcee749acfc8b6d7c199d4
Gareth Smith <gvs.0027@gmail.com>
2020-07-10 18:15:59 +0100

    configure index.js for heroku

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const serveStatic = require("serve-static");
 const bodyParser = require("body-parser");
 const cors = require('cors')
 const db = require("./queries");
 
 const app = express();
+app.use(serveStatic(__dirname + "/dist"));
 
 app.use(cors())
 
@@ -18,10 +20,6 @@ app.get("/easeofcaremenudata", db.getEaseOfCareMenuData);
 app.get("/petsafemenudata", db.getPetSafeMenuData);
 
 app.get("/menuTitles", db.getMenuTitles);
-
-app.get("/", (request, response) => {
-  response.json({ info: "Node.js, Express, and Postgres API" });
-});
 
 app.use(express.static('dist'))
 

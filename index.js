b81e9946622f81de14e54c86f6dbae6fabf75d72
Gareth Smith <gvs.0027@gmail.com>
2020-07-14 16:16:41 +0100

    Edit axios request for Heroku

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-const db = (require("./queries"));
+const db = require("./queries");
 
 const app = express();
 
-app.get(/.*/, function (req, res) {
-  res.sendfile(__dirname + "/dist/index.html");
-})
+// app.get(/.*/, function(req, res) {
+//   res.sendfile(__dirname + "/dist/index.html");
+// });
 
-app.use(express.static(__dirname + "/dist/"))
+app.use(express.static(__dirname + "/dist/"));
 
 app.use(cors());
 
@@ -26,9 +26,8 @@ app.get("/petsafemenudata", db.getPetSafeMenuData);
 
 app.get("/menuTitles", db.getMenuTitles);
 
-app.use(express.static("dist"));
-
 const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
   console.log(`Listening on ${port}`);
 });

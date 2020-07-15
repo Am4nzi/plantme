c936ffe4351f53736a2c5c942832978678d97b2b
Gareth Smith <gvs.0027@gmail.com>
2020-07-15 03:11:07 +0100

    configure API call for Heroku Postgres

--- src/App.vue
+++ src/App.vue
@@ -33,7 +33,7 @@ export default {
 
     if (window.location.href.includes("localhost")) {
       dataBaseUrl = "http://localhost:3000/api";
-    } else dataBaseUrl = "/api";
+    } else dataBaseUrl = `${process.env.VUE_APP_DATABASE_URL}/api`;
 
     Vue.axios
       .get(`${dataBaseUrl}/sizemenudata`)

a3a4b5d3cdcfa21443838a70536bb6726d1d7230
Am4nzi <gvs.0027@gmail.com>
2020-08-09 15:18:02 +0100

    add initial styling to results page

--- src/views/Results.vue
+++ src/views/Results.vue
@@ -21,40 +21,108 @@
       </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar app color="indigo" dark>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title>Application</v-toolbar-title>
+    <v-app-bar app flat color="white">
+      <v-app-bar-nav-icon
+        @click.stop="drawer = !drawer"
+        color="grey"
+      ></v-app-bar-nav-icon>
+      <v-toolbar-title>UNDER CONSTRUCTION</v-toolbar-title>
     </v-app-bar>
 
-    <v-main>
-      <v-container class="fill-height" fluid>
-        <v-row align="center" justify="center">
-          <v-col class="text-center">
-            <v-tooltip left>
-              <template v-slot:activator="{ on }">
-                <v-btn :href="source" icon large target="_blank" v-on="on">
-                  <v-icon large>mdi-code-tags</v-icon>
-                </v-btn>
-              </template>
-              <span>Source</span>
-            </v-tooltip>
+    <v-item-group
+      v-model="selected"
+      :mandatory="mandatory"
+      :multiple="multiple"
+    >
+      <v-container>
+        <v-row class="mb-4">
+          <Heading />
+        </v-row>
+        <v-row class="right-margin">
+          <v-col
+            v-for="item in getMenuData.plantSize"
+            :key="item.id"
+            class="d-flex"
+            :class="{ active: item === activeItem }"
+            cols="6"
+            xs="12"
+            sm="6"
+            md="6"
+            lg="6"
+            xl="6"
+          >
+            <v-item v-slot:default="{ active, toggle }">
+              <v-card
+                :color="active ? '#fcc7b8' : ''"
+                outlined
+                tile
+                class="mx-auto"
+                max-width="385"
+                height="385"
+                @click="
+                  toggle();
+                  selectItem(item, 'setPlantSizeMenuSelection');
+                "
+              >
+                <v-hover v-slot:default="{ hover }">
+                  <v-img :src="item.image" height="385" class="align-end">
+                    <v-card color="#fcc7b8" tile>
+                      <v-card-title v-if="hover">
+                        Plant Name
+                      </v-card-title>
+                    </v-card>
+                  </v-img>
+                </v-hover>
+              </v-card>
+            </v-item>
           </v-col>
         </v-row>
       </v-container>
-    </v-main>
-    <v-footer color="indigo" app>
-      <span class="white--text">&copy; {{ new Date().getFullYear() }}</span>
+    </v-item-group>
+
+    <v-footer color="#c4f5e1" app>
+      <span class="grey--text">&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
 </template>
 
 <script>
 export default {
+  name: "Results",
+  components: {},
   props: {
     source: String
   },
   data: () => ({
-    drawer: null
-  })
+    drawer: null,
+    mandatory: false,
+    multiple: true,
+    selected: [],
+    activeItem: null,
+    menu: {
+      plantSizeData: [],
+      previousSelection: ""
+    }
+  }),
+  computed: {
+    getMenuData() {
+      return this.$store.getters.getMenuData;
+    }
+  },
+  watch: {
+    multiple(val) {
+      this.selected = val
+        ? this.selected >= 0
+          ? [this.selected]
+          : []
+        : this.selected.pop();
+    }
+  }
 };
 </script>
+
+<style>
+.right-margin {
+  margin: 0 18% 0 13%;
+}
+</style>
